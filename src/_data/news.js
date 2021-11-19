6b66c2f218f9830ddebc5227085e6e5adf4cb808
jeremiegiffard <e132784f@gmail.com>
2021-11-19 22:55:54 +0100

    Adding dynamic data

---
+++ src/_data/news.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+require('dotenv').config();
+
+
+
+module.exports = async function(){
+    try{
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=fr&category=sports&pageSize=5&apiKey=${process.env.API_KEY}`);
+        return response.data;
+    } catch (error){
+        console.error(error);
+    }
+};
