f1a35e513a23e377a6946cb62dbec04f86c94973
jeremiegiffard <e132784f@gmail.com>
2021-11-20 18:05:01 +0100

    rewrite

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,26 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require('dotenv').config();
+
+
+async function getNews(country){
+    try{
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&category=sports&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`);
+        return {
+            "country": country,
+            "articles":  response.data.articles
+        }
+    } catch (error){
+        console.error(error);
+    }
+};
 
 
 
 module.exports = async function(){
-    try{
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=fr&category=sports&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`);
-        return response.data;
-    } catch (error){
-        console.error(error);
-    }
+    let newsPromises = countries.map(getNews);
+    return Promise.all(newsPromises).then( newsObjects => {
+        console.log('newsObjects :', newsObjects);
+        return [].concat.apply([],newsObjects);
+    });
 };
